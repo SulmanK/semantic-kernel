SulmanK/semantic-kernel

commit 3de42c933dfaffccdade48be1d70e5902760421c

.Net: Represent all embeddings as `ReadOnlyMemory<float>` (#2419)
### Motivation and Context

Across AI-related libraries, we'd like a consistent representation of an
embedding. It needs to support efficient handling of the data, with the
ability to get a span over the data so that the data can be manipulated
efficiently, which rules out interfaces like `IEnumerable<float>` and
`IList<float>`. It should ideally encourage immutability but not enforce
it, such that developers have escape hatches that don't require them to
continually allocate and copy around large objects. It should be usable
in both synchronous and asynchronous contexts. And it should be a
ubiquitous core type that's available everywhere. Based on that,
`ReadOnlyMemory<float>` is the winning candidate.

### Description

This PR overhauls SK to use `ReadOnlyMemory<float>` for embeddings. In
addition to the advantages listed above, I think it also cleans things
up nicely.

Note that in the System.Text.Json v8.0.0 nuget package, ReadOnlyMemory
is implicitly supported. Once SK moves from having a v6 to a v8
dependency, we can delete the ReadOnlyMemoryConverter and all mentions
of it.

### Contribution Checklist

<!-- Before submitting this PR, please make sure: -->

- [x] The code builds clean without any errors or warnings
- [x] The PR follows the [SK Contribution
Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
and the [pre-submission formatting
script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
raises no violations
- [x] All unit tests pass, and I have added new tests where possible
- [ ] I didn't break anyone :smile:

Co-authored-by: Shawn Callegari <36091529+shawncal@users.noreply.github.com>

dotnet/src/Connectors/Connectors.Memory.Postgres/PostgresMemoryStore.cs
@@ -1,11 +1,12 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.SemanticKernel.AI.Embeddings;
 using Microsoft.SemanticKernel.Diagnostics;
 using Microsoft.SemanticKernel.Memory;
 using Npgsql;
@@ -135,7 +136,7 @@ public class PostgresMemoryStore : IMemoryStore
     /// <inheritdoc/>
     public async IAsyncEnumerable<(MemoryRecord, double)> GetNearestMatchesAsync(
         string collectionName,
-        Embedding<float> embedding,
+        ReadOnlyMemory<float> embedding,
         int limit,
         double minRelevanceScore = 0,
         bool withEmbeddings = false,
@@ -150,7 +151,7 @@ public class PostgresMemoryStore : IMemoryStore
 
         IAsyncEnumerable<(PostgresMemoryEntry, double)> results = this._postgresDbClient.GetNearestMatchesAsync(
             tableName: collectionName,
-            embedding: new Vector(embedding.Vector.ToArray()),
+            embedding: new Vector(GetOrCreateArray(embedding)),
             limit: limit,
             minRelevanceScore: minRelevanceScore,
             withEmbeddings: withEmbeddings,
@@ -163,7 +164,7 @@ public class PostgresMemoryStore : IMemoryStore
     }
 
     /// <inheritdoc/>
-    public async Task<(MemoryRecord, double)?> GetNearestMatchAsync(string collectionName, Embedding<float> embedding, double minRelevanceScore = 0, bool withEmbedding = false,
+    public async Task<(MemoryRecord, double)?> GetNearestMatchAsync(string collectionName, ReadOnlyMemory<float> embedding, double minRelevanceScore = 0, bool withEmbedding = false,
         CancellationToken cancellationToken = default)
     {
         return await this.GetNearestMatchesAsync(
@@ -187,7 +188,7 @@ public class PostgresMemoryStore : IMemoryStore
             tableName: collectionName,
             key: record.Key,
             metadata: record.GetSerializedMetadata(),
-            embedding: new Vector(record.Embedding.Vector.ToArray()),
+            embedding: new Vector(GetOrCreateArray(record.Embedding)),
             timestamp: record.Timestamp?.UtcDateTime,
             cancellationToken: cancellationToken).ConfigureAwait(false);
 
@@ -198,10 +199,16 @@ public class PostgresMemoryStore : IMemoryStore
     {
         return MemoryRecord.FromJsonMetadata(
             json: entry.MetadataString,
-            embedding: entry.Embedding != null ? new Embedding<float>(entry.Embedding!.ToArray()) : Embedding<float>.Empty,
+            embedding: entry.Embedding?.ToArray() ?? ReadOnlyMemory<float>.Empty,
             key: entry.Key,
             timestamp: entry.Timestamp?.ToLocalTime());
     }
+
+    private static float[] GetOrCreateArray(ReadOnlyMemory<float> memory) =>
+        MemoryMarshal.TryGetArray(memory, out ArraySegment<float> array) &&
+        array.Count == array.Array!.Length ?
+            array.Array :
+            memory.ToArray();
 
     #endregion
 }

dotnet/src/Connectors/Connectors.UnitTests/Memory/Postgres/PostgresMemoryStoreTests.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.SemanticKernel.AI.Embeddings;
 using Microsoft.SemanticKernel.Connectors.Memory.Postgres;
 using Microsoft.SemanticKernel.Memory;
 using Moq;
@@ -272,7 +271,7 @@ public class PostgresMemoryStoreTests
     private void AssertMemoryRecordEqual(MemoryRecord expectedRecord, MemoryRecord actualRecord)
     {
         Assert.Equal(expectedRecord.Key, actualRecord.Key);
-        Assert.Equal(expectedRecord.Embedding.Vector, actualRecord.Embedding.Vector);
+        Assert.True(expectedRecord.Embedding.Span.SequenceEqual(actualRecord.Embedding.Span));
         Assert.Equal(expectedRecord.Metadata.Id, actualRecord.Metadata.Id);
         Assert.Equal(expectedRecord.Metadata.Text, actualRecord.Metadata.Text);
         Assert.Equal(expectedRecord.Metadata.Description, actualRecord.Metadata.Description);
@@ -281,10 +280,10 @@ public class PostgresMemoryStoreTests
         Assert.Equal(expectedRecord.Metadata.ExternalSourceName, actualRecord.Metadata.ExternalSourceName);
     }
 
-    private MemoryRecord GetRandomMemoryRecord(Embedding<float>? embedding = null)
+    private MemoryRecord GetRandomMemoryRecord(ReadOnlyMemory<float>? embedding = null)
     {
         var id = Guid.NewGuid().ToString();
-        var memoryEmbedding = embedding ?? new Embedding<float>(new[] { 1f, 3f, 5f });
+        var memoryEmbedding = embedding ?? new[] { 1f, 3f, 5f };
 
         return MemoryRecord.LocalRecord(
             id: id,
@@ -301,7 +300,7 @@ public class PostgresMemoryStoreTests
         return new PostgresMemoryEntry()
         {
             Key = memoryRecord.Key,
-            Embedding = new Pgvector.Vector(memoryRecord.Embedding.Vector.ToArray()),
+            Embedding = new Pgvector.Vector(memoryRecord.Embedding.ToArray()),
             MetadataString = memoryRecord.GetSerializedMetadata(),
             Timestamp = memoryRecord.Timestamp?.UtcDateTime
         };

commit 765f227cd0927b0402467bf943ca8b8596da6ae4

.Net: Postgres memory store add simple constructor (#2688)
### Motivation and Context

<!-- Thank you for your contribution to the semantic-kernel repo!
Please help reviewers and future users, providing the following
information:
  1. Why is this change required?
  2. What problem does it solve?
  3. What scenario does it contribute to?
  4. If it fixes an open issue, please link to the issue here.
-->

The Postgres memory store adds a simple constructor using a connection
string. The `NpgsqlDataSource` is managed internally by the postgres
memory store.

### Description

<!-- Describe your changes, the overall approach, the underlying design.
These notes will help understanding how your code works. Thanks! -->

- Similar to the recent changes to the redis memory store. which allows
the user to pass the connection string directly. the `NpgsqlDataSource`
is managed internally.
- Fixed postgres integration test (caused by #2419)

### Contribution Checklist

<!-- Before submitting this PR, please make sure: -->

- [x] The code builds clean without any errors or warnings
- [x] The PR follows the [SK Contribution
Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
and the [pre-submission formatting
script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
raises no violations
- [x] All unit tests pass, and I have added new tests where possible
- [x] I didn't break anyone :smile:

---------

Co-authored-by: Lee Miller <lemiller@microsoft.com>
Co-authored-by: Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>

dotnet/src/Connectors/Connectors.Memory.Postgres/PostgresKernelBuilderExtensions.cs
@@ -16,9 +16,30 @@ public static class PostgresKernelBuilderExtensions
     /// Registers Postgres Memory Store.
     /// </summary>
     /// <param name="builder">The <see cref="KernelBuilder"/> instance</param>
+    /// <param name="connectionString">Postgres database connection string.</param>
+    /// <param name="vectorSize">Embedding vector size.</param>
+    /// <param name="schema">Schema of collection tables.</param>
+    /// <returns>Self instance</returns>
+    public static KernelBuilder WithPostgresMemoryStore(this KernelBuilder builder,
+        string connectionString,
+        int vectorSize,
+        string schema = PostgresMemoryStore.DefaultSchema)
+    {
+        builder.WithMemoryStorage((loggerFactory) =>
+        {
+            return new PostgresMemoryStore(connectionString, vectorSize, schema);
+        });
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Registers Postgres Memory Store.
+    /// </summary>
+    /// <param name="builder">The <see cref="KernelBuilder"/> instance</param>
     /// <param name="dataSource">Postgres data source.</param>
     /// <param name="vectorSize">Embedding vector size.</param>
-    /// <param name="schema">Schema of collection tables. The default value is "public".</param>
+    /// <param name="schema">Schema of collection tables.</param>
     /// <returns>Self instance</returns>
     public static KernelBuilder WithPostgresMemoryStore(this KernelBuilder builder,
         NpgsqlDataSource dataSource,

dotnet/src/Connectors/Connectors.Memory.Postgres/PostgresMemoryStore.cs
@@ -11,6 +11,7 @@ using Microsoft.SemanticKernel.Diagnostics;
 using Microsoft.SemanticKernel.Memory;
 using Npgsql;
 using Pgvector;
+using Pgvector.Npgsql;
 
 namespace Microsoft.SemanticKernel.Connectors.Memory.Postgres;
 
@@ -21,16 +22,30 @@ namespace Microsoft.SemanticKernel.Connectors.Memory.Postgres;
 /// The embedded data is saved to the Postgres database specified in the constructor.
 /// Similarity search capability is provided through the pgvector extension. Use Postgres's "Table" to implement "Collection".
 /// </remarks>
-public class PostgresMemoryStore : IMemoryStore
+public class PostgresMemoryStore : IMemoryStore, IDisposable
 {
     internal const string DefaultSchema = "public";
 
     /// <summary>
     /// Initializes a new instance of the <see cref="PostgresMemoryStore"/> class.
     /// </summary>
+    /// <param name="connectionString">Postgres database connection string.</param>
+    /// <param name="vectorSize">Embedding vector size.</param>
+    /// <param name="schema">Database schema of collection tables.</param>
+    public PostgresMemoryStore(string connectionString, int vectorSize, string schema = DefaultSchema)
+    {
+        NpgsqlDataSourceBuilder dataSourceBuilder = new(connectionString);
+        dataSourceBuilder.UseVector();
+        this._dataSource = dataSourceBuilder.Build();
+        this._postgresDbClient = new PostgresDbClient(this._dataSource, schema, vectorSize);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PostgresMemoryStore"/> class.
+    /// </summary>
     /// <param name="dataSource">Postgres data source.</param>
     /// <param name="vectorSize">Embedding vector size.</param>
-    /// <param name="schema">Database schema of collection tables. The default value is "public".</param>
+    /// <param name="schema">Database schema of collection tables.</param>
     public PostgresMemoryStore(NpgsqlDataSource dataSource, int vectorSize, string schema = DefaultSchema)
         : this(new PostgresDbClient(dataSource, schema, vectorSize))
     {
@@ -181,9 +196,28 @@ public class PostgresMemoryStore : IMemoryStore
             cancellationToken: cancellationToken).FirstOrDefaultAsync(cancellationToken).ConfigureAwait(false);
     }
 
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        this.Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Disposes the managed resources.
+    /// </summary>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            this._dataSource?.Dispose();
+        }
+    }
+
     #region private ================================================================================
 
     private readonly IPostgresDbClient _postgresDbClient;
+    private readonly NpgsqlDataSource? _dataSource;
 
     private async Task<string> InternalUpsertAsync(string collectionName, MemoryRecord record, CancellationToken cancellationToken)
     {

dotnet/src/Connectors/Connectors.UnitTests/Memory/Postgres/PostgresMemoryStoreTests.cs
@@ -33,7 +33,7 @@ public class PostgresMemoryStoreTests
     public async Task ItCanCreateCollectionAsync()
     {
         // Arrange
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         await store.CreateCollectionAsync(CollectionName);
@@ -46,7 +46,7 @@ public class PostgresMemoryStoreTests
     public async Task ItCanDeleteCollectionAsync()
     {
         // Arrange
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         await store.DeleteCollectionAsync(CollectionName);
@@ -59,7 +59,7 @@ public class PostgresMemoryStoreTests
     public async Task ItReturnsTrueWhenCollectionExistsAsync()
     {
         // Arrange
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         var doesCollectionExist = await store.DoesCollectionExistAsync(CollectionName);
@@ -78,7 +78,7 @@ public class PostgresMemoryStoreTests
             .Setup(client => client.DoesTableExistsAsync(CollectionName, CancellationToken.None))
             .ReturnsAsync(false);
 
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         var doesCollectionExist = await store.DoesCollectionExistAsync(CollectionName);
@@ -94,7 +94,7 @@ public class PostgresMemoryStoreTests
         var expectedMemoryRecord = this.GetRandomMemoryRecord();
         var postgresMemoryEntry = this.GetPostgresMemoryEntryFromMemoryRecord(expectedMemoryRecord)!;
 
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         var actualMemoryRecordKey = await store.UpsertAsync(CollectionName, expectedMemoryRecord);
@@ -115,7 +115,7 @@ public class PostgresMemoryStoreTests
         var batchUpsertMemoryRecords = new[] { memoryRecord1, memoryRecord2, memoryRecord3 };
         var expectedMemoryRecordKeys = batchUpsertMemoryRecords.Select(l => l.Key).ToList();
 
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         var actualMemoryRecordKeys = await store.UpsertBatchAsync(CollectionName, batchUpsertMemoryRecords).ToListAsync();
@@ -144,7 +144,7 @@ public class PostgresMemoryStoreTests
             .Setup(client => client.ReadAsync(CollectionName, expectedMemoryRecord.Key, true, CancellationToken.None))
             .ReturnsAsync(postgresMemoryEntry);
 
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         var actualMemoryRecord = await store.GetAsync(CollectionName, expectedMemoryRecord.Key, withEmbedding: true);
@@ -164,7 +164,7 @@ public class PostgresMemoryStoreTests
             .Setup(client => client.ReadAsync(CollectionName, MemoryRecordKey, true, CancellationToken.None))
             .ReturnsAsync((PostgresMemoryEntry?)null);
 
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         var actualMemoryRecord = await store.GetAsync(CollectionName, MemoryRecordKey, withEmbedding: true);
@@ -199,7 +199,7 @@ public class PostgresMemoryStoreTests
                 .Setup(client => client.ReadBatchAsync(CollectionName, memoryRecordKeys, true, CancellationToken.None))
                 .Returns(expectedMemoryRecords.Select(memoryRecord => this.GetPostgresMemoryEntryFromMemoryRecord(memoryRecord)).ToAsyncEnumerable());
 
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         var actualMemoryRecords = await store.GetBatchAsync(CollectionName, memoryRecordKeys, withEmbeddings: true).ToListAsync();
@@ -224,7 +224,7 @@ public class PostgresMemoryStoreTests
             .Setup(client => client.GetTablesAsync(CancellationToken.None))
             .Returns(expectedCollections.ToAsyncEnumerable());
 
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         var actualCollections = await store.GetCollectionsAsync().ToListAsync();
@@ -243,7 +243,7 @@ public class PostgresMemoryStoreTests
     {
         // Arrange
         const string MemoryRecordKey = "fake-record-key";
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         await store.RemoveAsync(CollectionName, MemoryRecordKey);
@@ -257,7 +257,7 @@ public class PostgresMemoryStoreTests
     {
         // Arrange
         string[] memoryRecordKeys = new string[] { "fake-record-key1", "fake-record-key2", "fake-record-key3" };
-        var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
+        using var store = new PostgresMemoryStore(this._postgresDbClientMock.Object);
 
         // Act
         await store.RemoveBatchAsync(CollectionName, memoryRecordKeys);

dotnet/src/IntegrationTests/Connectors/Memory/Postgres/PostgresMemoryStoreTests.cs
@@ -63,7 +63,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     [Fact(Skip = SkipReason)]
     public void InitializeDbConnectionSucceeds()
     {
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         // Assert
         Assert.NotNull(memoryStore);
     }
@@ -72,7 +72,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task ItCanCreateAndGetCollectionAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         string collection = "test_collection";
 
         // Act
@@ -88,7 +88,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task ItCanCheckIfCollectionExistsAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         string collection = "my_collection";
 
         // Act
@@ -103,7 +103,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task CollectionsCanBeDeletedAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         string collection = "test_collection";
         await memoryStore.CreateCollectionAsync(collection);
         Assert.True(await memoryStore.DoesCollectionExistAsync(collection));
@@ -119,7 +119,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task GetAsyncReturnsEmptyEmbeddingUnlessSpecifiedAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         MemoryRecord testRecord = MemoryRecord.LocalRecord(
             id: "test",
             text: "text",
@@ -146,7 +146,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task ItCanUpsertAndRetrieveARecordWithNoTimestampAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         MemoryRecord testRecord = MemoryRecord.LocalRecord(
             id: "test",
             text: "text",
@@ -176,7 +176,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task ItCanUpsertAndRetrieveARecordWithTimestampAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         MemoryRecord testRecord = MemoryRecord.LocalRecord(
             id: "test",
             text: "text",
@@ -207,7 +207,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task UpsertReplacesExistingRecordWithSameIdAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         string commonId = "test";
         MemoryRecord testRecord = MemoryRecord.LocalRecord(
             id: commonId,
@@ -231,7 +231,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
         Assert.NotNull(actual);
         Assert.Equal(testRecord.Metadata.Id, key);
         Assert.Equal(testRecord2.Metadata.Id, actual.Key);
-        Assert.True(testRecord.Embedding.Span.SequenceEqual(actual.Embedding.Span));
+        Assert.False(testRecord.Embedding.Span.SequenceEqual(actual.Embedding.Span));
         Assert.True(testRecord2.Embedding.Span.SequenceEqual(actual.Embedding.Span));
         Assert.NotEqual(testRecord.Metadata.Text, actual.Metadata.Text);
         Assert.Equal(testRecord2.Metadata.Description, actual.Metadata.Description);
@@ -241,7 +241,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task ExistingRecordCanBeRemovedAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         MemoryRecord testRecord = MemoryRecord.LocalRecord(
             id: "test",
             text: "text",
@@ -265,7 +265,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task RemovingNonExistingRecordDoesNothingAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         string collection = "test_collection";
 
         // Act
@@ -281,7 +281,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task ItCanListAllDatabaseCollectionsAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         string[] testCollections = { "random_collection1", "random_collection2", "random_collection3" };
         await memoryStore.CreateCollectionAsync(testCollections[0]);
         await memoryStore.CreateCollectionAsync(testCollections[1]);
@@ -311,7 +311,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task GetNearestMatchesReturnsAllResultsWithNoMinScoreAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         var compareEmbedding = new float[] { 1, 1, 1 };
         int topN = 4;
         string collection = "test_collection";
@@ -373,7 +373,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task GetNearestMatchAsyncReturnsEmptyEmbeddingUnlessSpecifiedAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         var compareEmbedding = new float[] { 1, 1, 1 };
         string collection = "test_collection";
         await memoryStore.CreateCollectionAsync(collection);
@@ -433,7 +433,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task GetNearestMatchAsyncReturnsExpectedAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         var compareEmbedding = new float[] { 1, 1, 1 };
         string collection = "test_collection";
         await memoryStore.CreateCollectionAsync(collection);
@@ -491,7 +491,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task GetNearestMatchesDifferentiatesIdenticalVectorsByKeyAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         var compareEmbedding = new float[] { 1, 1, 1 };
         int topN = 4;
         string collection = "test_collection";
@@ -526,7 +526,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task ItCanBatchUpsertRecordsAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         int numRecords = 10;
         string collection = "test_collection";
         IEnumerable<MemoryRecord> records = this.CreateBatchRecords(numRecords);
@@ -546,7 +546,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task ItCanBatchGetRecordsAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         int numRecords = 10;
         string collection = "test_collection";
         IEnumerable<MemoryRecord> records = this.CreateBatchRecords(numRecords);
@@ -566,7 +566,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task ItCanBatchRemoveRecordsAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         int numRecords = 10;
         string collection = "test_collection";
         IEnumerable<MemoryRecord> records = this.CreateBatchRecords(numRecords);
@@ -593,7 +593,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task DeletingNonExistentCollectionDoesNothingAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         string collection = "test_collection";
 
         // Act
@@ -604,7 +604,7 @@ public class PostgresMemoryStoreTests : IAsyncLifetime
     public async Task ItCanBatchGetRecordsAndSkipIfKeysDoNotExistAsync()
     {
         // Arrange
-        PostgresMemoryStore memoryStore = this.CreateMemoryStore();
+        using PostgresMemoryStore memoryStore = this.CreateMemoryStore();
         int numRecords = 10;
         string collection = "test_collection";
         IEnumerable<MemoryRecord> records = this.CreateBatchRecords(numRecords);