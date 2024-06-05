9ad793e6239c058cc171cb9eb8d042452b35854e
Sulman Khan <sulman@vt.edu>
2024-06-05 17:47:54 -0400

    ok

--- python/semantic_kernel/const.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-
-from typing import Final
-
-METADATA_EXCEPTION_KEY: Final[str] = "exception"
-DEFAULT_SERVICE_NAME: Final[str] = "default"
--- python/semantic_kernel/kernel_types.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-
-from typing import TypeVar
-
-from semantic_kernel.services.ai_service_client_base import AIServiceClientBase
-
-AI_SERVICE_CLIENT_TYPE = TypeVar("AI_SERVICE_CLIENT_TYPE", bound=AIServiceClientBase)
